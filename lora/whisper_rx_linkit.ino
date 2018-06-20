b272928fcce383de825c661f18b590421cfecdb3
eronides neto <eron93@gmail.com>
2018-06-19 22:56:42 -0300

    Update whisper_rx_linkit.ino

--- lora/whisper_rx_linkit.ino
+++ lora/whisper_rx_linkit.ino
@@ -1,22 +1,20 @@
-// Arduino9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
+// Eronides Neto - LoRa radio examples
+// Example sketch showing how to create a simple messaging radio communication with LoRa modules
 // with the RH_RF95 class. RH_RF95 class does not provide for addressing or
 // reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Arduino9x_TX
+// It is designed to work with LoRa Whisper board wisen.com.au/store/products/whisper-node-lora/
  
 #include <SPI.h>
 #include <RH_RF95.h>
 
 /*
- *#define RFM95_CS 8
-#define RFM95_RST 4
-#define RFM95_INT 7
- 
+// USE THIS FOR USE RX MODULE AS OTHER WHISPER LORA BOARD
+#define RFM95_CS 10
+#define RFM95_RST 7
+#define RFM95_INT 2
  */
 
-//PINOUT PARA LinKit.. 
+// USE THIS IF YOU WANT TO USE LINKIT SMART7688 DUO DEV BOARD
 #define RFM95_CS 8
 #define RFM95_RST 9
 #define RFM95_INT 7
